90bd0c3f91d6709275c722a46868f3711cb12f3f
b00009 <mwcothran@fullsail.edu>
2013-08-22 07:23:19 -0500

    global variables

--- Cothran_Michael_Project3.js
+++ Cothran_Michael_Project3.js
@@ -1,1 +1,16 @@
 //alert("JavaScript works!");
+/*
+ *Michael Cothran
+ *SDI 1308
+ *Project 3
+ *Rowen Journey
+ */
+
+
+//Global variables
+var witchName = "Rowen";
+var pedagogeName = "Gwendolyn";
+var numberOfSpells = 3;
+var witchMagick = 100;
+var spellBookName = pedagogeName + "'s Grimore";
+var familier = ["Shadow" , " Jinx"];
