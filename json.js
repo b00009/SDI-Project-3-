31deec7efc72a992f520cce51bc7b6fdb0b7ba05
b00009 <mwcothran@fullsail.edu>
2013-08-22 07:20:27 -0500

    json

---
+++ json.js
@@ -0,0 +1,25 @@
+/*
+*Michael Cothran
+*1308
+*Project 3
+*8/20/2013
+*new spells
+*/
+
+var bonus = {
+    "bonusSpells": [
+        {
+            "spellName": "lightning stike spell"
+        },
+        
+        {
+            "spellName": "hail spell"
+        },
+        
+        {
+            "spellName": "vanish spell"
+        },
+        
+    ]
+};
+
