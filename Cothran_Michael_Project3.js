e6caf45bab7286282928220710287de7cb0eaf2b
b00009 <mwcothran@fullsail.edu>
2013-08-22 08:04:34 -0500

    first of js

--- Cothran_Michael_Project3.js
+++ Cothran_Michael_Project3.js
@@ -14,3 +14,24 @@ var numberOfSpells = 3;
 var witchMagick = 100;
 var spellBookName = pedagogeName + "'s Grimore";
 var familier = ["Shadow" , " Jinx"];
+var familierAge = [1,50]
+
+//Object
+var witch = {
+    "name": "Rowen",
+ 	"hasRunes": false,
+    "runes": 8,
+    "experience": 0,
+    "ToLevel": 10,
+    "xp": 0,
+    "runesMax": 100,    
+    
+    
+};    
+   
+
+
+//story
+console.log("The young and beutiful " + witch.name + " has prepaired for her journey.");
+console.log(familier+ " and " + witchName + " begin the quest.");
+console.log("As the three set out on their journey " + witch.name + " asks... Jinx as a witch's familier you know her spells right?");
